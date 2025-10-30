d5af4bfd602c94a78a437a6d0a0351bb717f4a6a
David Thanphilom <davidthanphilom@msn.com>
2025-10-30 12:31:49 -0700

    updated

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,71 +1,9 @@
-import { config } from "dotenv";
-import express from "express";
-import cors from "cors";
-import { connectToDb } from "./db.js";
 import serverless from "serverless-http";
-
-config();
-// console.log(process.env.MONGODB_URI);
+import express from "express";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const uri = process.env.MONGODB_URI;
-
-console.log("from index.js");
-
-
-// let year = new Date().getFullYear();
-// const dbName = `halloweenDB_${year}`;
-// const collectionName = "families";
-
-// const collection = await connectToDb(uri, dbName, collectionName);
-
-let collection;
-const connectDb = async () => {
-  if (!collection) {
-    const year = new Date().getFullYear();
-    const dbName = `halloweenDB_${year}`;
-    const collectionName = "families";
-    collection = await connectToDb(uri, dbName, collectionName);
-    console.log("Database connected and collection set");
-    console.log(collection);
-  }
-};
 
 app.get("/hello", (req, res) => res.send("hello"));
 
-app.get("/", async (req, res) => {
-  console.log("GET / request received");
-  try {
-    await connectDb();
-    const families = await collection.find({}).toArray();
-    // const families = await collection.find({}).toArray();
-    res.json(families);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
-});
-
-app.post("/submit", async (req, res) => {
-  console.log("Received data:", req.body);
-  // res.json({ message: 'Data received!', data: req.body });
-
-  const familyData = req.body;
-  try {
-    await connectDb();
-    await collection.insertOne(familyData); // insert data directly
-    res.json({ message: "Family added!", data: familyData });
-  } catch (err) {
-    res.status(500).json({ error: "Failed to insert data" });
-  }
-});
-
-
-// Wrap the Express app with serverless-http
 const handler = serverless(app);
-
-// Export the handler for Netlify
 export { handler };
