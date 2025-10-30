312560097b9a77a1377585f0dc9052fd6fba639f
David Thanphilom <davidthanphilom@msn.com>
2025-10-29 19:17:53 -0700

    updated

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
-import { connectToDb } from "db.js";
+import { connectToDb } from "./db.js";
 import serverless from "serverless-http";
 
 config();
